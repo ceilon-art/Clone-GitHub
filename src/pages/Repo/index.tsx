03c488e56ca0e62c07a7a22bda8ae73cb70b8760
Andre Luiz <andreluizkanaan222@gamil.com>
2020-09-18 20:22:51 -0300

    Developing

--- src/pages/Repo/index.tsx
+++ src/pages/Repo/index.tsx
@@ -1,9 +1,53 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-// import { Container } from './styles';
+import {
+  Container,
+  Breadcrum,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from "./styles";
 
 const Repo: React.FC = () => {
-  return <div />;
+  return (
+    <Container>
+      <Breadcrum>
+        <RepoIcon />
+        <Link className={"username"} to={"/ceilon-art"}>
+          ceilon-art
+        </Link>
+        <span>/</span>
+        <Link className={"reponame"} to={"/ceilon-art/Flex-Movie"}>
+          Flex-Movie
+        </Link>
+      </Breadcrum>
+
+      <p>Clone of Netflix</p>
+
+      <Stats>
+        <li>
+          <StarIcon />
+          <b>9</b>
+          <span>stars</span>
+        </li>
+
+        <li>
+          <ForkIcon />
+          <b>0</b>
+          <span>forks</span>
+        </li>
+      </Stats>
+
+      <LinkButton href={"https://github.com/ceilon-art/Flex-Movie"}>
+        <GithubIcon />
+        <span>View on GitHub</span>
+      </LinkButton>
+    </Container>
+  );
 };
 
 export default Repo;
